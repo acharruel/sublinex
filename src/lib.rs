a6de8074aa2f3fd9b06d201deba7630c13fcaf26
Adrien Charruel <adrien.charruel@tagheuer.com>
2024-03-13 10:38:22 +0100

    Extend Timestamp from srtlib to work in milliseconds

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use srtlib::{Subtitles, Timestamp};
+
 pub struct Config {
     input_file: String,
     output_file: String,
@@ -17,8 +19,53 @@ impl Config {
     }
 }
 
+trait TimestampExt {
+    fn to_ms(&self) -> u64;
+}
+
+impl TimestampExt for Timestamp {
+    fn to_ms(&self) -> u64 {
+        let (hours, minutes, seconds, milliseconds) = self.get();
+        hours as u64 * 3600 * 1000
+            + minutes as u64 * 60 * 1000
+            + seconds as u64 * 1000
+            + milliseconds as u64
+    }
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     println!("Input file: {}", config.input_file);
     println!("Output file: {}", config.output_file);
+
+    let subs = Subtitles::parse_from_file(&config.input_file, None)?;
+
+    for s in subs {
+        println!(
+            "ts: {} --> {} ms",
+            s.start_time, s.start_time.to_ms(),
+        );
+    }
+
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_timestamp_to_ms() {
+        let ts = Timestamp::new(0, 0, 0, 0);
+        assert_eq!(ts.to_ms(), 0);
+        let ts = Timestamp::new(0, 0, 0, 1);
+        assert_eq!(ts.to_ms(), 1);
+        let ts = Timestamp::new(0, 0, 1, 0);
+        assert_eq!(ts.to_ms(), 1000);
+        let ts = Timestamp::new(0, 1, 0, 0);
+        assert_eq!(ts.to_ms(), 60000);
+        let ts = Timestamp::new(1, 0, 0, 0);
+        assert_eq!(ts.to_ms(), 3600000);
+        let ts = Timestamp::new(1, 1, 1, 1);
+        assert_eq!(ts.to_ms(), 3661001);
+    }
 }
